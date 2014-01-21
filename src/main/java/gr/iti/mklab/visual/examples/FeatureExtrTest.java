9bf59c8926138816455ea5b19cffc7a179dac12f
Katerina <katerina@seksycode.com>
2014-01-21 15:02:42 +0200

    test

--- src/main/java/gr/iti/mklab/visual/examples/FeatureExtrTest.java
+++ src/main/java/gr/iti/mklab/visual/examples/FeatureExtrTest.java
@@ -1,7 +1,59 @@
 package gr.iti.mklab.visual.examples;
 
+import gr.iti.mklab.visual.aggregation.VladAggregatorMultipleVocabularies;
+import gr.iti.mklab.visual.dimreduction.PCA;
+import gr.iti.mklab.visual.extraction.AbstractFeatureExtractor;
+import gr.iti.mklab.visual.extraction.SURFExtractor;
+import gr.iti.mklab.visual.vectorization.ImageVectorization;
+import gr.iti.mklab.visual.vectorization.ImageVectorizationResult;
+
+import java.util.Arrays;
+
 /**
  * Created by katerina on 1/21/14.
+ * Just a test class
  */
 public class FeatureExtrTest {
+
+    public static void main(String[] args) throws Exception {
+
+        String learningFolder = "/home/katerina/workspace/learning_files/";
+
+        String imageFolder = "/home/katerina/Desktop/";
+        String imageFilename = "work.jpg";
+        int targetLengthMax = 1024;
+        int maxNumPixels = 768 * 512;
+
+        int[] numCentroids = {128, 128, 128, 128};
+        int initialLength = numCentroids.length * numCentroids[0] * AbstractFeatureExtractor.SURFLength;
+
+        String[] codebookFiles = {
+                learningFolder + "surf_l2_128c_0.csv",
+                learningFolder + "surf_l2_128c_1.csv",
+                learningFolder + "surf_l2_128c_2.csv",
+                learningFolder + "surf_l2_128c_3.csv"
+        };
+
+        String pcaFile = learningFolder + "pca_surf_4x128_32768to1024.txt";
+
+        //Initialize the ImageVectorization
+        ImageVectorization.setFeatureExtractor(new SURFExtractor());
+        ImageVectorization.setVladAggregator(new VladAggregatorMultipleVocabularies(codebookFiles,
+                numCentroids, AbstractFeatureExtractor.SURFLength));
+
+        if (targetLengthMax < initialLength) {
+            PCA pca = new PCA(targetLengthMax, 1, initialLength, true);
+            pca.loadPCAFromFile(pcaFile);
+            ImageVectorization.setPcaProjector(pca);
+        }
+
+        ImageVectorization imvec = new ImageVectorization(imageFolder, imageFilename, targetLengthMax, maxNumPixels);
+
+        ImageVectorizationResult imvr = imvec.call();
+        double[] vector = imvr.getImageVector();
+        System.out.println(Arrays.toString(vector));
+
+    }
 }
+
+
